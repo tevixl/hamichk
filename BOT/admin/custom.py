411e3203d9e36504c11b0a68108b42fcb48daf26
Am Tevi <tevisolh@gmail.com>
2025-03-03 09:49:02 +0300

    Update custom.py

--- BOT/admin/custom.py
+++ BOT/admin/custom.py
@@ -10,7 +10,7 @@ async def cmd_cs(Client, message):
         OWNER_ID    = json.loads(open("FILES/config.json", "r" , encoding="utf-8").read())["OWNER_ID"]
         if user_id not in OWNER_ID:
             resp = """<b>You Don't Have Permission To Use This Command.    
-Contact Bot Owner @nairobiangoon !</b>"""
+Contact Bot Owner @tevixl !</b>"""
             await message.reply_text(resp, message.id)
             return
 
